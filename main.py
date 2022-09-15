837433b0c8e9dbfda9151e3738bd21035eeb74bd
TimothyFothergill <67912934+TimothyFothergill@users.noreply.github.com>
2022-09-15 17:10:47 +0100

    G2C-003 adds debug option and limits

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from random import randrange
 from dotenv import load_dotenv
 load_dotenv()
 
-def generate_giphy(query, rating="g", num=10, random=True, github=False):
+def generate_giphy(query, rating="g", num=10, random=True, github=False, outputOnly=False):
     url = "http://api.giphy.com/v1/gifs/search"
     params = parse.urlencode({
         "q"         : query,
@@ -22,6 +22,8 @@ def generate_giphy(query, rating="g", num=10, random=True, github=False):
     if(random):
         with request.urlopen("".join((url, "?", params))) as response:
             data = json.loads(response.read())
+            if(num > len(data)):
+                num == len(data)
             n = randrange(0,num)
             print(
                 json.dumps(data["data"][n]["title"] + 
@@ -30,10 +32,16 @@ def generate_giphy(query, rating="g", num=10, random=True, github=False):
             )
             clip = json.dumps(data["data"][n]["images"]["original"]["url"])
             clip = clip.strip('\"')
-            if(github):
-                pyperclip.copy("![" + query + "](" + clip + ")")
+            if(outputOnly == False):
+                if(github):
+                    pyperclip.copy("![" + query + "](" + clip + ")")
+                else:
+                    pyperclip.copy(clip)
             else:
-                pyperclip.copy(clip)
+                if(github):
+                    print("![" + query + "](" + clip + ")\n")
+                else:
+                    print(clip+"\n")
 
     else:
         for n in range(num):
@@ -51,15 +59,17 @@ def main(argv):
     num = 10
     random=True
     github=False
+    outputOnly=False
 
     try:
-        opts, args = getopt.getopt(argv,"hs:r:n:R:g",["search=", "rating=", "num=", "random-disabled", "github"])
+        opts, args = getopt.getopt(argv,"hs:r:n:Rgd",["search=", "rating=", "num=", "random-disabled", "github", "output-only"])
     except getopt.GetoptError:
-        print('main.py -s <search> -r <rating> -n <number> -R <random-disabled> -g <github>')
+        print('main.py -s <search> -r <rating> -n <number> -R <random-disabled> -g <github> -d <output-only>')
         sys.exit(2)
+
     for opt, arg in opts:
         if opt == '-h':
-            print('main.py -s <search> -r <rating> -n <number> -R <random-disabled> -g <github>')
+            print('main.py -s <search> -r <rating> -n <number> -R <random-disabled> -g <github> -d <output-only>')
             sys.exit(2)
         elif opt in ("-s", "--search"):
             s = arg
@@ -71,7 +81,9 @@ def main(argv):
             random = False
         elif opt in ("-g", "--github"):
             github = True
-    generate_giphy(s,r,int(num),random,github)
+        elif opt in ("-d", "--output-only"):
+            outputOnly = True
+    generate_giphy(s,r,int(num),random,github,outputOnly)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
